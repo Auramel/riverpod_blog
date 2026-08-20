9cd5616e7874e53b136d8c7f5592ffb246df0a9f
auramel <auramel@yandex.ru>
2026-08-20 19:42:40 +0500

    feat: обновить провайдер homeCommands для автоматического освобождения ресурсов

--- lib/modules/home/providers.dart
+++ lib/modules/home/providers.dart
@@ -3,4 +3,4 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:river_blog/core/base/empty_state.dart';
 import 'package:river_blog/modules/home/commands/home_commands.dart';
 
-final NotifierProvider<HomeCommands, EmptyState> homeCommandsProvider = NotifierProvider<HomeCommands, EmptyState>(HomeCommands.new);
+final NotifierProvider<HomeCommands, EmptyState> homeCommandsProvider = NotifierProvider.autoDispose<HomeCommands, EmptyState>(HomeCommands.new);
